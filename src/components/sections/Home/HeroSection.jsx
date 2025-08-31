db2b9c9846112eff4c679634df5edfd4ffbabbd9
snbon <sweaniz@icloud.com>
2025-08-31 21:35:55 +0200

    live V1.0

--- src/components/sections/Home/HeroSection.jsx
+++ src/components/sections/Home/HeroSection.jsx
@@ -9,7 +9,7 @@ const HeroSection = () => {
           From Concept to Product to Impact
         </h1>
         <p className="text-base-robotic md:text-lg-robotic lg:text-xl-robotic text-gray-700 max-w-3xl mx-auto mb-8 leading-relaxed">
-          A software studio dedicated to turning bold ideas into impactful digital products. We design and build SaaS solutions that simplify, scale, and solve real business challenges.
+          A Software studio dedicated to transforming bold ideas into impactful digital products. <br/> We focus on simplicity, speed, and solving real business challenges.
         </p>
         <div className="flex flex-col sm:flex-row gap-4 justify-center">
           <Link to="/cases" className="btn-secondary text-sm-robotic">
